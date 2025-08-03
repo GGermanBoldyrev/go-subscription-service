3f7f2a0546afb79385301543858ba1089f8fc297
German Boldyrev <boldyrev_german@mail.ru>
2025-08-04 01:13:55 +0300

    feat: log middleware

--- internal/app/router.go
+++ internal/app/router.go
@@ -1,9 +1,14 @@
 package app
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	"go-subscription-service/internal/middleware"
+)
 
 func SetupRouter(container *AppContainer) *chi.Mux {
 	router := chi.NewRouter()
+
+	router.Use(middleware.RequestLogger)
 
 	container.SubscriptionHandler.RegisterRoutes(router)
 
---
+++ internal/middleware/request_logger.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"go-subscription-service/pkg/logger"
+	"net/http"
+	"time"
+)
+
+func RequestLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		ww := &statusWriter{ResponseWriter: w, status: 200}
+
+		next.ServeHTTP(ww, r)
+
+		logger.Infof("HTTP %s %s %d %s", r.Method, r.URL.Path, ww.status, time.Since(start))
+	})
+}
+
+type statusWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (w *statusWriter) WriteHeader(status int) {
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
+}
